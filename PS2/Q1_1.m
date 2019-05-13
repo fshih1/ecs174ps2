bac11793828c057fcc6e1d47e5bcb552d81c937c
Ez <fshih@ucdavis.edu>
2019-05-13 15:02:07 -0700

    (a) done, should be correct

--- PS2/Q1_1.m
+++ PS2/Q1_1.m
@@ -1,5 +1,11 @@
 % create an interface, to match the images. create pairs, that match in the
 % two images. 8-10 pairs
-function [pairs] = Q1_1(image1,image2)
+function [t1,t2] = Q1_1(image1,image2)
 img1 = imread(image1);
 img2 = imread(image2);
+imshow(img1)
+[col1,row1] = ginput(8)
+imshow(img2)
+[col2,row2] = ginput(8)
+t1 = [col1 row1]'
+t2 = [col2 row2]'
