997154bdf2447209ad6050cb5a76d4e20be35ead
Ez <fshih@ucdavis.edu>
2019-05-13 14:24:50 -0700

    first commit

---
+++ PS2/Q1_1.m
@@ -0,0 +1,5 @@
+% create an interface, to match the images. create pairs, that match in the
+% two images. 8-10 pairs
+function [pairs] = Q1_1(image1,image2)
+img1 = imread(image1);
+img2 = imread(image2);
---
+++ PS2/computeH.m
@@ -0,0 +1,1 @@
+function H = computeH(t1, t2) %
