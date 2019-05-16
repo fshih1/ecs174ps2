cc583fdad70dff66da671097e8a1ce35bd7ca559
Squeezy <percevalfeng123@gmail.com>
2019-05-15 21:33:35 -0700

    Update warpImage.m

--- PS2/warpImage.m
+++ PS2/warpImage.m
@@ -2,20 +2,20 @@ function [warpIm, mergeIm] = warpImage(inputIm, refIm, H)
 
     % refIm = 'crop2.jpg'
 
-    warpImg = [];
+    warpIm = [];
 
-    [nrow,ncol,~] = size(refIm);
+    [nrow,ncol,~] = size(refIm)
     for row = 1:nrow
         for col = 1:ncol
+            disp(row);
             temp = inv(H) * [row,col,1]';
             x_prime = temp(1) / temp(3);  %we might need to scale it back?
             y_prime = temp(2) / temp(3);
 
         %        if floor(x_prime) ~= ceil(x_prime) % we need to do interpolation
-            warpImg(i, j, 1) = interp2(inputIm(:,:,1), x_prime, y_prime); 
-            warpImg(i, j, 2) = interp2(inputIm(:,:,2), x_prime, y_prime);
-            warpImg(i, j, 3) = interp2(inputIm(:,:,3), x_prime, y_prime);
-
+            warpIm(row, col, 1) = interp2(inputIm(:,:,1), x_prime, y_prime); 
+            warpIm(row, col, 2) = interp2(inputIm(:,:,2), x_prime, y_prime);
+            warpIm(row, col, 3) = interp2(inputIm(:,:,3), x_prime, y_prime);
 
     %        else % we chilling
     %            warpImg(x_prime,y_prime,1) = refIm(row,col,1);
@@ -24,4 +24,4 @@ function [warpIm, mergeIm] = warpImage(inputIm, refIm, H)
     %        end
    end
 end
-imshow(warpImg)
+imshow(warpIm)
