ac6550080f69f5f39ed3fb948d63ed9ee4f84d7d
Squeezy <percevalfeng123@gmail.com>
2019-05-16 22:12:43 -0700

    ez

--- PS2/warpImage.m
+++ PS2/warpImage.m
@@ -1,23 +1,42 @@
 function [warpIm, mergeIm] = warpImage(inputIm, refIm, H)
     warpIm = [];
     mergeIm = [];
-
-    a = [];
-    b = [];
     
     [nrow,ncol,~] = size(refIm);
+    [maxRow, maxCol, ~] = size(inputIm);
+    
     for row = 1:nrow
         for col = 1:ncol
             temp = inv(H) * [row,col,1]';
             x_prime = temp(1) / temp(3);
             y_prime = temp(2) / temp(3);
-
-            a(row, col) = x_prime;
-            b(row, col) = y_prime;
+            
+            x_max = ceil(x_prime);
+            x_min = floor(x_prime);
+            y_max = ceil(y_prime);
+            y_min = floor(y_prime);
+            
+            if col == 1 & col == 1
+                disp(x_min);
+                disp(y_min);
+            end
+            
+            a = x_prime - x_min;
+            b = y_prime - y_min;
+            
+            if x_min < 1 || y_min < 1
+                warpIm(row, col) = 0;
+                continue
+            end
+            if x_max > maxRow || y_max > maxCol
+                warpIm(row, col) = 0;
+                continue
+            end
+            warpIm(row, col, 1) = (1-a)*(1-b)*inputIm(x_min, y_min, 1) + a*(1-b)*inputIm(x_max, y_min, 1) + a*b*inputIm(x_max, y_max, 1) + (1-a)*b*inputIm(x_min, y_max, 1);
+            warpIm(row, col, 2) = (1-a)*(1-b)*inputIm(x_min, y_min, 2) + a*(1-b)*inputIm(x_max, y_min, 2) + a*b*inputIm(x_max, y_max, 2) + (1-a)*b*inputIm(x_min, y_max, 2);
+            warpIm(row, col, 3) = (1-a)*(1-b)*inputIm(x_min, y_min, 3) + a*(1-b)*inputIm(x_max, y_min, 3) + a*b*inputIm(x_max, y_max, 3) + (1-a)*b*inputIm(x_min, y_max, 3);
         end
     end
-   
-    warpIm(:,:, 1) = interp2(double(inputIm(:,:,1)), x_prime, y_prime);
-    warpIm(:,:, 2) = interp2(double(inputIm(:,:,2)), x_prime, y_prime);
-    warpIm(:,:, 3) = interp2(double(inputIm(:,:,3)), x_prime, y_prime);
+ 
+    
 end
--- PS2/warpImage2.m
+++ PS2/warpImage2.m
@@ -21,7 +21,7 @@ function [warpIm, mergeIm] = warpImage2(inputIm, refIm, H)
 %             
 %         end
 %     end
-    [A, B] = meshgrid(1:ncol_inp, 1:nrow_inp)
+    [A, B] = meshgrid(1:ncol_inp, 1:nrow_inp);
     
     for row = 1:nrow_ref
         for col = 1:ncol_ref
@@ -29,8 +29,8 @@ function [warpIm, mergeIm] = warpImage2(inputIm, refIm, H)
 %             B(row, col) = row;
             
             temp = inv(H) * [row,col,1]';
-            x_prime = temp(1) / temp(3);
-            y_prime = temp(2) / temp(3);
+            x_prime = temp(1,1) / temp(3,1);
+            y_prime = temp(2,1) / temp(3,1);
             a(row, col) = y_prime;
             b(row, col) = x_prime;
         end
@@ -42,10 +42,8 @@ function [warpIm, mergeIm] = warpImage2(inputIm, refIm, H)
     if size(A) ~= size(inputIm(:,:,1))
         error ('Input size mismatch')
     end
-     
-    wrapIm = interp2(A, B, double(inputIm(:,:,1)), a, b)
-%     wrapIm(:,:,2) = interp2(A, B, double(inputIm(:,:,2)), a, b);
-%     wrapIm(:,:,3) = interp2(A, B, double(inputIm(:,:,3)), a, b);
     
-
+    wrapIm = interp2(A, B, double(inputIm(:,:,1)), a, b);
+    wrapIm(:,:,2) = interp2(A, B, double(inputIm(:,:,2)), a, b);
+    wrapIm(:,:,3) = interp2(A, B, double(inputIm(:,:,3)), a, b);
 end
