708b1bf990a3247fdf35af85a8d58e6c1d162ee6
Squeezy <percevalfeng123@gmail.com>
2019-05-18 11:52:40 -0700

    Update testWarp.m

--- PS2/testWarp.m
+++ PS2/testWarp.m
@@ -1,9 +1,8 @@
+% USAGE:
+% Use pts.m to get H and save H as global in workspace so that you can use this script without
+% having to click points every time you run it
+
 [warpedImg, mergedImg] = warpImage(inputIm, refIm, H);
 
-% warpedImg = resultImgs(1, 1);
-% mergedImg = resultImgs(2, 1);
 imshow(warpedImg);
-% hold on;
-% line([0, 1], [50, 50], 'Color', 'red', 'Linewidth', 20);
-% line([355, 355], [1, 2], 'Color', 'red', 'Linewidth', 2000);
 
